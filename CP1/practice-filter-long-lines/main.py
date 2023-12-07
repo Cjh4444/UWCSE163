7ec2ffbe752f0cda6e1abfff93b0ab94372c60e3
Camden Harris <camdenharris@gmail.com>
2023-12-07 07:40:30 -0800

    finished filter-long-lines

---
+++ CP1/practice-filter-long-lines/main.py
@@ -0,0 +1,19 @@
+def filter_long_lines(file_name, min_words):
+    """
+    Prints each line in the file with the given name that has
+    at least the given number of words in that line.
+    """
+    # Write your code here :)
+
+    with open("CP1/practice-filter-long-lines/" + file_name, "r") as f:
+        for line in f:
+            if len(line.split()) >= min_words:
+                print(line, end="")
+
+
+def main():
+    filter_long_lines("song.txt", 7)
+
+
+if __name__ == "__main__":
+    main()
