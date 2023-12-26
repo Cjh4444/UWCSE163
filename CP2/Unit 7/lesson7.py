60738e2bb712094c661fb30f33254f9ab76a70ac
Camden Harris <camdenharris@gmail.com>
2023-12-25 22:04:31 -0800

    got all the problems in a testable format for peck

--- CP2/Unit 7/lesson7.py
+++
@@ -1,1 +0,0 @@
-def problem_0(df):
---
+++ CP2/Unit7/lesson7.py
@@ -0,0 +1,23 @@
+def problem_0(df):
+    return df["magnitude"].mean()
+
+
+def problem_1(df):
+    return len(df[df["magnitude"] > problem_0(df)])
+
+
+def problem_2(df):
+    is_tonga = df["name"] == "Tonga"
+    is_papua_new_guinea = df["name"] == "Papua New Guinea"
+   #ll ohewqeweq
+    tonga_papua_df = df[is_tonga | is_papua_new_guinea]
+
+    is_8th_of_month = tonga_papua_df["day"] == 8
+
+    return tonga_papua_df[is_8th_of_month]
+
+
+def problem_3(df):
+    max_idx = df["magnitude"].idxmax()
+
+    return df.iloc[max_idx]
