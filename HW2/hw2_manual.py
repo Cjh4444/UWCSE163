050e417987f355fa8bef94e8bddee1029b9454bc
Camden Harris <camdenharris@gmail.com>
2024-03-12 08:58:19 -0700

    HW2 should be complete, just need to add comments

--- HW2/hw2_manual.py
+++ HW2/hw2_manual.py
@@ -1,4 +1,49 @@
 """
-Your information here
+Camden Harris
+CSE 163 AX
+This program implements the functions for HW0
 """
-# Your code here!
+
+
+def species_count(data: list) -> int:
+    return len({pokemon["name"] for pokemon in data})
+
+
+def max_level(data: list) -> tuple:
+    max_level = 0
+    max_name = ""
+    for pokemon in data:
+        level = pokemon["level"]
+        if level > max_level:
+            max_level = level
+            max_name = pokemon["name"]
+    return max_name, max_level
+
+
+def filter_range(data: list, lower: int, upper: int) -> list:
+    return [
+        pokemon["name"]
+        for pokemon in data
+        if lower <= pokemon["level"] < upper
+    ]
+
+
+def mean_attack_for_type(data: list, type: str) -> float:
+    attack_list = [
+        pokemon["atk"] for pokemon in data if pokemon["type"] == type
+    ]
+    return sum(attack_list) / len(attack_list)
+
+
+def count_types(data: list) -> dict:
+    type_dictionary = dict()
+    for pokemon in data:
+        type_dictionary[pokemon["type"]] = (
+            type_dictionary.get(pokemon["type"], 0) + 1
+        )
+    return type_dictionary
+
+
+def mean_attack_per_type(data: list) -> dict:
+    types_set = {pokemon["type"] for pokemon in data}
+    return {type: mean_attack_for_type(data, type) for type in types_set}
--- HW2/hw2_pandas.py
+++ HW2/hw2_pandas.py
@@ -1,4 +1,35 @@
 """
-Your information here
+Camden Harris
+CSE 163 AX
+This program implements the functions for HW0
 """
-# Your code here!
+
+from pandas import DataFrame
+
+
+def species_count(df: DataFrame) -> int:
+    return len(df["name"].unique())
+
+
+def max_level(df: DataFrame) -> tuple:
+    max_idx = df["level"].idxmax()
+    return df["name"].iloc[max_idx], df["level"].iloc[max_idx]
+
+
+def filter_range(df: DataFrame, lower: int, upper: int) -> list:
+    is_above_min = lower <= df["level"]
+    is_below_max = upper > df["level"]
+    return list(df[is_above_min & is_below_max]["name"])
+
+
+def mean_attack_for_type(df: DataFrame, type: str) -> float:
+    is_fire = df["type"] == type
+    return df[is_fire]["atk"].mean()
+
+
+def count_types(df: DataFrame) -> dict:
+    return dict(df["type"].value_counts())
+
+
+def mean_attack_per_type(df: DataFrame) -> dict:
+    return dict(df.groupby("type")["atk"].mean())
