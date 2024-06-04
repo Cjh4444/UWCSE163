8fa395ebb109e7b488f0f62640243f5320f0039a
Camden Harris <camdenharris@gmail.com>
2024-06-03 20:37:10 -0700

    HW0_Correction (#17)
    * added the minimal documentation i missed, need to check with peck if that's all he wanted
    
    * fixed test case function documentation

--- HW0/hw0_test.py
+++ HW0/hw0_test.py
@@ -10,6 +10,9 @@ from cse163_utils import assert_equals
 
 
 def test_total():
+    """
+    Runs test cases for test_total function
+    """
     # The regular case
     assert_equals(15, hw0.total(5))
     # Seems likely we could mess up 0 or 1
@@ -18,6 +21,9 @@ def test_total():
 
 
 def test_funky_sum():
+    """
+    Runs test cases for funky_sum function
+    """
     assert_equals(1, hw0.funky_sum(1, 3, 0))
     assert_equals(3, hw0.funky_sum(1, 3, 1))
     assert_equals(1.5, hw0.funky_sum(1, 3, 0.25))
@@ -26,17 +32,23 @@ def test_funky_sum():
 
 
 def test_swip_swap():
-    assert_equals('offbar', hw0.swip_swap('foobar', 'f', 'o'))
-    assert_equals('foocar', hw0.swip_swap('foobar', 'b', 'c'))
-    assert_equals("ginbgonb", hw0.swip_swap("bingbong", 'b', 'g'))
-    assert_equals("olo", hw0.swip_swap("lol", 'l', 'o'))
+    """
+    Runs test cases for swip_swap function
+    """
+    assert_equals("offbar", hw0.swip_swap("foobar", "f", "o"))
+    assert_equals("foocar", hw0.swip_swap("foobar", "b", "c"))
+    assert_equals("ginbgonb", hw0.swip_swap("bingbong", "b", "g"))
+    assert_equals("olo", hw0.swip_swap("lol", "l", "o"))
 
 
 def main():
+    """
+    main function to run all test cases
+    """
     test_total()
     test_funky_sum()
     test_swip_swap()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
