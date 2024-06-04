8bfb1f43e7e090b7cdcfd7b5aaf77e503762b82b
Camden Harris <camdenharris@gmail.com>
2024-06-03 21:48:14 -0700

    this is probably good, if peck bonks me so be it

--- CP9/lesson26/my_set_client.py
+++ CP9/lesson26/my_set_client.py
@@ -1,16 +1,18 @@
-# TODO: Need an import statement here
 from lesson26.my_set import MySet
 
 
 def main():
     s = MySet()
+    real_set = set()
     nums = [14, 2, 17, 2]
     query = [14, 2, 17, 12, 4]
     for n in nums:
         s.add(n)
+        real_set.add(n)
     print("Length of set:", len(s))
     for n in query:
-        print("Set contains", n, "=>:", n in s)
+        print("MySet contains", n, "=>:", n in s)
+        print("RealSet contains", n, "=>:", n in real_set)
 
 
 if __name__ == "__main__":
